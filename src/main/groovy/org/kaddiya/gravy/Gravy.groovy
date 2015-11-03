6b850b54f1a885dab62a2cbe1b6a698252dda157
Ameya <Webonise@Ameyas-MacBook-Pro.local>
2015-11-03 17:01:44 +0530

    Finsihed with the basic command

--- src/main/groovy/org/kaddiya/gravy/Gravy.groovy
+++ src/main/groovy/org/kaddiya/gravy/Gravy.groovy
@@ -6,10 +6,20 @@ import groovy.util.logging.Slf4j
 /**
  * Created by Webonise on 03/11/15.
  */
-@Slf4j
+
 @CompileStatic
 class Gravy {
-    public static void main(String[]args ){
-        log.info("main method called")
+    public static void main(String[ ]args ){
+       println("Welcome to the Dev Kitchen,the Groovy way.Please help us with your order!")
+        assert args.size() >= 1 : "Please provide the recipe to cook!"
+        List<String> argsList = Arrays.asList(args)
+        String parentArg = argsList.get(0)
+        switch (parentArg.toUpperCase()){
+            case "COOK":
+                println("Lets bootstrap your application")
+                break;
+            default:
+                throw new IllegalArgumentException("Top level argument not supported")
+        }
     }
 }
