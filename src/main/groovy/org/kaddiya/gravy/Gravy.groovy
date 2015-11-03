d46ce35cf76f9361bb1dd799839ab70c4e3f3799
Ameya <Webonise@Ameyas-MacBook-Pro.local>
2015-11-03 15:50:12 +0530

    Added the basic classes

---
+++ src/main/groovy/org/kaddiya/gravy/Gravy.groovy
@@ -0,0 +1,15 @@
+package org.kaddiya.gravy
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+
+/**
+ * Created by Webonise on 03/11/15.
+ */
+@Slf4j
+@CompileStatic
+class Gravy {
+    public static void main(String[]args ){
+        log.info("main method called")
+    }
+}
---
+++ src/test/groovy/org/kaddiya/gravy/GravySpec.groovy
@@ -0,0 +1,9 @@
+package org.kaddiya.gravy
+
+import spock.lang.Specification
+
+/**
+ * Created by Webonise on 03/11/15.
+ */
+class GravySpec extends Specification {
+}
